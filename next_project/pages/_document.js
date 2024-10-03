5555395b48ed19c7459692f088b2a0014eabb845
EunJi Kim <dust9629@gmail.com>
2024-10-03 14:59:05 +0900

    Fix : Update viewport settings

--- next_project/pages/_document.js
+++ next_project/pages/_document.js
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        href="https://cdn.jsdelivr.net/npm/pretendard/dist/web/static/pretendard.css"
-        rel="stylesheet"
-      />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/pretendard/dist/web/static/pretendard.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
